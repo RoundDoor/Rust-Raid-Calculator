9068f22d3981b7727778c7378ad74f20bc0ccd8f
RoundDoor <Leprouty112@gmail.com>
2023-10-24 00:52:23 -0400

    added some needed final vars to calculator that will be used to calculate raid cost

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,44 @@
 public class Calculator {
 
-    final int ROCKETDMG;
-    final int C4DMG;
-    final int EXPODMG;
-    final int SATCHELDMG;
-    final int BEANCANDMG;
+    private final int ROCKETDMG;
+    private final int C4DMG;
+    private final int EXPODMG;
+    private final int SATCHELDMG;
+    private final int BEANCANDMG;
+    private final int MEATALWALL;
+    private final int ARMOREDWALL;
+    private final int STONEWALL;
+    private final int WOODWALL;
+    private final int ARMOREDDOOR;
+    private final int SHEETMETALDOOR;
+    private final int GARAGEDOOR;
 
+    public int Rocket() {
+        return 0;
+    }
+
+    public int C4() {
+        return 0;
+    }
+
+    public int Expo() {
+        return 0;
+    }
+
+    public int Satchel() {
+        return 0;
+    }
+
+    public int BeanCan() {
+        return 0;
+    }
+
+    public String CheapestTotal() {
+        return null;
+    }
+
+    public String CheapestOne() {
+        return null;
+    }
 
 }
